3416dc6892e4974f9c678e71a3ff7ccf681c3e27
Miriam Cukier <cukierm@gmail.com>
2015-06-24 19:19:30 -0400

    Version of AngryBoss that works.

--- AngryBoss.rb
+++ AngryBoss.rb
@@ -1,6 +1,3 @@
 puts "What?  What do you want?"
 response = gets.chomp
-puts "WHADDAYA MEAN \"" + response.upcase + "\"?!? YOU'RE FIRED!!  
-
-
-I SWEAR, EVERYONE SAYS \"" + response.upcase + "\" THESE DAYS."
+puts "WHADDAYA MEAN \"" + response.upcase + "\"?!? YOU'RE FIRED!! I SWEAR, EVERYONE SAYS \"" + response.upcase + "\" THESE DAYS."
