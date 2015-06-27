7e1d692ecfd1c7451459e867a624fd7bff32b9f0
Miriam Cukier <cukierm@gmail.com>
2015-06-27 11:28:02 -0400

    Boring Bar Date, have to say BYE three times, but ugly nested conditionals.

--- Boring_Bar_Date.rb
+++ Boring_Bar_Date.rb
@@ -1,13 +1,26 @@
+counter = 0
+
 puts "WHAT DID YOU SAY?"
 
 response = gets.chomp
 
-while response != "BYE"
+while counter < 3
 
-	if response.upcase == response
+	if response == "BYE" 
+		if counter == 2 
+			puts "FINE.  YOU'RE BORING, ANYWAY."
+			break
+		else
+			counter = counter + 1
+			puts "NICE TRY, BUT I STILL CAN'T HEAR YOU."
+	end
+
+	elsif response.upcase == response
 		puts "NO, NOT SINCE " + (rand(15) + 1984).to_s + "!"
+
 	else
 		puts "WHAT'S THAT?  I CAN'T HEAR YOU!!!  THE MUSIC'S TOO LOUD!"
+
 	end
 
 	response = gets.chomp
